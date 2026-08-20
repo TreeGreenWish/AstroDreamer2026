3dfdf50a44160c430e92da90f3c7ddcbe527582d
Tyler McCardle <68253755+TreeGreenWish@users.noreply.github.com>
2026-08-20 16:59:39 -0500

    Mount journal backup control

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import DreamAiRetryControls from './components/DreamAiRetryControls';
+import { ExportJournalButton } from './components/ExportJournalButton';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <DreamAiRetryControls />
+    <ExportJournalButton />
   </StrictMode>,
 );
