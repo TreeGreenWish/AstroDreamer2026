54c2d5ae3b2e92c31437d129c1d5b44b72ff1f76
Tyler McCardle <68253755+TreeGreenWish@users.noreply.github.com>
2026-08-21 01:03:51 -0500

    Gate AstraDream behind authenticated session

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import DreamAiRetryControls from './components/DreamAiRetryControls';
 import { ExportJournalButton } from './components/ExportJournalButton';
+import { AuthGate } from './components/AuthGate';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <DreamAiRetryControls />
-    <ExportJournalButton />
+    <AuthGate>
+      <App />
+      <DreamAiRetryControls />
+      <ExportJournalButton />
+    </AuthGate>
   </StrictMode>,
 );
