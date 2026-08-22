2d6c66e6763848a69ca1b42e97df563d2b30e94d
Tyler McCardle <68253755+TreeGreenWish@users.noreply.github.com>
2026-08-22 02:42:50 -0500

    Load local date-only compatibility fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import './lib/localDateOnly';
 import App from './App.tsx';
 import DreamAiRetryControls from './components/DreamAiRetryControls';
 import { ExportJournalButton } from './components/ExportJournalButton';
